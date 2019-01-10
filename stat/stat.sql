7a99393c9886cd700d0cf750be8f2a896d568b85
SongSunJoo <mundalling@naver.com>
2019-01-10 11:57:37 +0900

    stat.sql ; 붙임

--- stat/stat.sql
+++ stat/stat.sql
@@ -16,62 +16,62 @@ create table statUser (
 	oldDB_id	number(10) references oldDB(oldDB_id),
 	mobileDB_id	number(10) references mobileDB(mobileDB_id),
 	user_nick	varchar2(100)
-)
+);
 
 나이DB
 create table ageDB (
 	ageDB_id	number(10)	primary key not null,
 	ageDB_value	varchar2(100) not null
-)
+);
 
 반려동물DB
 create table petDB (
 	petDB_id	number(10)	primary key not null,
 	petDB_value	varchar2(100)	not null
-)
+);
 
 성씨DB
 create table lastNameDB (
 	lnDB_id	number(10) primary key not null,
 	lnDB_value	varchar2(100) not null
-)
+);
 
 계층이동DB
 create table transferDB (
 	transDB_id	number(10) primary key not null,
 	transDB_value	varchar2(100) not null
-)
+);
 
 디지털 의존도 DB
 create table digitalDB (
 	digitalDB_id	number(10) primary key not null,
 	digitalDB_value	varchar2(100) not null
-)
+);
 
 행복지수 DB
 create table happyDB (
 	happyDB_id	number(10) primary key not null,
 	happyDB_value	varchar2(100) not null
-)
+);
 
 여행지1DB (대륙)
 create table tour1DB (
 	tourDB_id1	varchar2(100) primary key not null
-)
+);
 
 여행지2DB (나라)
 create table tour2DB (
 	tourDB_id2	varchar2(100) primary key not null
-)
+);
 
 나이 들었을 때 DB
 create table oldDB (
 	oldDB_id	number(10) primary key not null,
 	oldDB_value	varchar2(100) not null
-)
+);
 
 모바일 컨텐츠 DB
 create table mobileDB (
 	mobileDB_id	number(10) primary key not null,
 	mobileDB_value	varchar2(100) not null
-)
+);
